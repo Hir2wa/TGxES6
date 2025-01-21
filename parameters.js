a80b2c57967d7b7ed83e4c0472daaacda16f23bc
Alain <alainfabricehirwa@gmail.com>
2025-01-21 15:30:58 +0200

    ..

--- parameters.js
+++ parameters.js
@@ -17,3 +17,11 @@ console.log(average(2, 6));
 console.log(average(2, 3, 3, 5, 7, 10));
 console.log(average(7, 1432, 12, 13, 100));
 console.log(average());
+
+//Memory Efficiency:
+
+// If the method were defined inside the constructor, each instance would get a separate copy.
+// By using the prototype, all instances share the same method, saving memory.
+// Prototype Chain:
+
+// If you access a property/method on an object and it doesn't exist, JavaScript looks up the prototype chain to find it.
