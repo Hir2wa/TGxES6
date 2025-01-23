6e83f7cd838b2866ac8c0747e035451fa5b3ce27
sir-serge <irkzsrg@gmail.com>
2025-01-23 15:28:29 +0200

    ..

---
+++ sets.js
@@ -0,0 +1,4 @@
+const months = new Set(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+console.log(months.size);
+console.log(months.has('January'));
+console.log(months.values());
