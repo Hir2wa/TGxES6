a856b4a803d315da6f0dfc3adacb0270f3a5e9e6
Hir2wa <alainfabricehirwa@gmail.com>
2025-01-28 16:15:22 +0200

    ..

--- regex.js
+++ regex.js
@@ -3,3 +3,8 @@ let sentence = "  these  is  my sentence ";
 // used
 let regex = /is/;
 console.log(regex.test(sentence));
+
+
+function dup(s) {
+    return s.map(x => x.replace(/(.)\1+/g,'$1'))
+  };
