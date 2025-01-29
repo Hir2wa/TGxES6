eb5ae5cc379c091ef53758b3c69b5eaa361c7545
Hir2wa <alainfabricehirwa@gmail.com>
2025-01-29 15:32:39 +0200

    ..

--- callbackPromiss.js
+++ callbackPromiss.js
@@ -11,11 +11,23 @@ function waitWithCallback(callback) {
 
    function checkNumber(n){
 
-    return Promise((resolve,reject)=>{
+    return  new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            
+        
        if (n>10) {
          resolve("Number is large")
        } else{
         reject("Number is too small")
        }
+    }, 1000);
     })
    }
+
+   checkNumber(11)
+   .then((message)=>{
+  console.log(message)
+   })
+   .catch((error)=>{
+     console.log(error)
+   })
